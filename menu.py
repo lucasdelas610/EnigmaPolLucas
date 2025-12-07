49fe72aa5e35ffeffa701cf1c69ab38475f07f4f
Lucas <lucasdelas610@gmail.com>
2025-12-07 19:10:52 +0100

    añadimos el archivo menu para inicio

---
+++ menu.py
@@ -0,0 +1,7 @@
+def mostrar_menu():
+    print("ENIGMA:")
+    print("-------------------------")
+    print("1. Xifrar missatge")
+    print("2. Desxifrar missatge")
+    print("3. Editar rotors")
+    print("4. Sortir")
