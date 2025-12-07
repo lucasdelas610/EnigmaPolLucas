7dc05a9d8a053f22dcf71a5f92c936271b747c09
Lucas <lucasdelas610@gmail.com>
2025-12-07 18:47:47 +0100

    menu

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-pol = 1
+def mostrar_menu():
+    print()
+    print("ENIGMA:")
+    print("-------------------------")
+    print("1. Xifrar missatge")
+    print("2. Desxifrar missatge")
+    print("3. Editar rotors")
+    print("4. Sortir")
+    print()
+
+# Codi principal
+while True:
+    mostrar_menu()
+    opcio = input("Tria una opcio: ")
+    
+    if opcio == '4':
+        print("Has sortit del programa")
+        break
