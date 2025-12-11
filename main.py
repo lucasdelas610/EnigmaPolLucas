52a1d7a2175f2addb3e788e6c1eb903c66c26a3f
Lucas <lucasdelas610@gmail.com>
2025-12-11 14:12:56 +0100

    canvis als comentaris

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import menu
 import funciones 
 
-def inicio():
-    # Cargamos rotores
-    r1 = funciones.cargar_rotor("Rotor1.txt")
+def inicio(): #en aquest arxiu main tenim el codi net amb nomes una funcio per anar trucant les funcions de l'altre arxiu funciones 
+    r1 = funciones.cargar_rotor("Rotor1.txt") #carreguem tots els rotors
     r2 = funciones.cargar_rotor("Rotor2.txt")
     r3 = funciones.cargar_rotor("Rotor3.txt")
     print("Rotores cargados")
@@ -15,36 +14,31 @@ def inicio():
         if opcio == '1':
             print("Has triat xifrar...")
             
-            # 1. Inputs
-            pos1 = input("Lletra Rotor 1: ").upper()
+            pos1 = input("Lletra Rotor 1: ").upper() #demanen les lletres dels rotos
             pos2 = input("Lletra Rotor 2: ").upper()
             pos3 = input("Lletra Rotor 3: ").upper()
             print("Configuracio guardada: " + pos1 + " " + pos2 + " " + pos3)
 
             print("Llegint el missatge...")
             
-            # 2. Leemos archivo (Si no existe, el programa se cierra con error, igual que antes)
-            f = open("text/Missatge.txt", "r")
+            f = open("text/Missatge.txt", "r") #llegeix l'arxiu de text de missatge (hola mundo)
             missatge = f.read()
             f.close()
             print("Missatge original: " + missatge)
             
-            print("Limpiando el mensaje...")
-            missatge_net = funciones.preprocesar_missatge(missatge)
+            missatge_net = funciones.preprocesar_missatge(missatge) #truca la funcio preprocessar missatge
             print("Mensaje limpio: " + missatge_net)
-
-            print("Xifrant missatge...")
             
             # 3. Llamamos a la logica
-            resultado = funciones.xifrar_logica(missatge_net, r1, r2, r3, pos1, pos2, pos3)
+            resultado = funciones.xifrar_logica(missatge_net, r1, r2, r3, pos1, pos2, pos3) #ara truca la funcio per poder xifrar el missatge
             
-            print("RESULTAT FINAL: " + resultado)
+            print("Missatge xifrat: " + resultado)
             
-            # 4. Guardamos
-            f_salida = open("text/Xifrat.txt", "w")
+            f_salida = open("text/Xifrat.txt", "w") #aqui es guarda el resultat del missatge xifrat
             f_salida.write(resultado)
             f_salida.close()
             print("Guardado en text/Xifrat.txt")
+
 
         elif opcio == '2':
             print("Has triat desxifrar...")
@@ -60,8 +54,7 @@ def inicio():
             f.close()
             print("Missatge xifrat original: " + missatge)
             
-            # Llamamos a la logica
-            resultado = funciones.desxifrar_logica(missatge, r1, r2, r3, pos1, pos2, pos3)
+            resultado = funciones.desxifrar_logica(missatge, r1, r2, r3, pos1, pos2, pos3) #truquem la funcio de desxifrar el missatge
             
             print("MISSATGE ORIGINAL: " + resultado)
             
@@ -70,34 +63,34 @@ def inicio():
             f_salida.close()
             print("Guardado en text/desxifrat.txt")
             
-        elif opcio == '3':
-            rotor = input("Quin rotor vols editar? (1, 2, 3): " )
-            nom_fitxer = ""
+            
+        elif opcio == '3': #opcio d'editar rotors 
+            rotor = input("Quin rotor vols editar? (1, 2, 3): " ) #esculls el rotor que vulguis
+            nom_fitxer = "" 
 
             if rotor == '1': nom_fitxer = "text/Rotor1.txt"
             elif rotor == '2': nom_fitxer = "text/Rotor2.txt"
             elif rotor == '3': nom_fitxer = "text/Rotor3.txt"
             
             if nom_fitxer != "": 
-                nou_cablejat = input("Escriu la nova llista de 26 lletres: ").upper()
+                nou_cablejat = input("Escriu la nova llista de 26 lletres: ").upper() #demanem combinacio de lletres per canviar un rotor
                 notch = input("Escriu la lletra del Notch: ").upper()
                 
-                # Llamamos a la funcion (guardara lo que sea, aunque este mal)
-                funciones.guardar_rotor_modificat(nom_fitxer, nou_cablejat, notch)
+                funciones.guardar_rotor_modificat(nom_fitxer, nou_cablejat, notch) #truquem aquesta funcio per guardar-la
                 print("Rotor guardat.")
                 
-                # Recargamos
-                r1 = funciones.cargar_rotor("Rotor1.txt")
+          
+                r1 = funciones.cargar_rotor("Rotor1.txt") #tornem a carregar tots els rotors
                 r2 = funciones.cargar_rotor("Rotor2.txt")
                 r3 = funciones.cargar_rotor("Rotor3.txt")
             else:
                 print("Rotor incorrecte.")
 
-        elif opcio == '4':
-            print("Has sortit del programa")
+        elif opcio == '4': #l'opcio mes senzilla, nomes surt del programa al escollir-la
+            print("Has sortit del programa") 
             break
         else:
-            print("Opcio no valida.")
+            print("Opcio no valida.") #si escrius un altre numeroq ue no sigui de l'1 al 4, apareix aquest missatge
 
 if __name__ == "__main__":
     inicio()
